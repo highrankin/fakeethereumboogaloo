0d584a03535407ce5cdce4713ce326bdc98a015a
William Rankin <will@threshold.fm>
2021-03-13 13:45:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [prizeFund, setPrizeFund] = useState("");
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(async () => {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const prizeFund = await lottery.methods.getPrizeFund().call();
+    setManager(manager);
+    setPlayers(players);
+    setPrizeFund(prizeFund);
+  }, []);
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage("Waiting for transaction success");
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, "ether")
+    });
+    setMessage("You have been entered");
+  };
+
+  const pickWinner = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage("Waiting for transaction success");
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    setMessage("We have a winner");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2>Lottery Contract</h2>
+      <p>This contract is manged by {manager}</p>
+      <p>
+        The following {players.length} players are playing for a prize frund of{" "}
+        {web3.utils.fromWei(prizeFund, "ether")} Ether
+      </p>
+
+      <hr />
+      <form onSubmit={onSubmit}>
+        <h4>Wanna try your luck</h4>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "40%"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <label>Amount of Ether to enter</label>
+          <input
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          ></input>
+        </div>
+        <button type="submit">Enter</button>
+      </form>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={pickWinner}>Pick</button>
+      <hr />
+      <h1>{message}</h1>
     </div>
   );
 }
---
+++ src/lottery.js
@@ -0,0 +1,68 @@
+import web3 from "./web3";
+
+const address = "0xF1D382532cc1B912f22DE456C7161AD262f76849";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "getPrizeFund",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
